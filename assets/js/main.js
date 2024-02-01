d683e00d0a3ec03c849509ebc94c18823b3e7cf6
Fathima Azha <azhastudyz132@gmail.com>
2024-02-01 20:26:42 +0530

    Log In

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,36 @@
-function showLogInModal(){
+function showLogInModal() {
     //alert('ok')
 
     const loginModal = document.getElementById('login-modal');
     const boostrapModal = new bootstrap.Modal(loginModal);
     boostrapModal.show();
+}
+
+
+function logIn() {
+    //alert("ok");
+
+    let email = document.getElementById("login-email").value;
+    let password = document.getElementById("login-password").value;
+
+    // alert (email);
+    // alert (password);
+
+    const form = new FormData();
+    form.append("email", email);
+    form.append("password", password);
+
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState == 4) {
+            const response = request.responseText;
+            if (request === 'Success'){
+            alert('Log In Success');
+             } else {
+                alert(response)
+             }
+        }
+    };
+    request.open("post", "logInProcess.php", true);
+    request.send(form);
 }
--- model/Connection.php
+++ model/Connection.php
@@ -1,27 +1,27 @@
 <?php
 
-class Connection
+class Database
 {
     public static $connection;
 
     public static function setUpConnection()
     {
 
-        if (!isset(Connection::$connection)) {
-            Connection::$connection = new mysqli("localhost", "root", "Fa@azha1", "", "3306");
+        if (!isset(Database::$connection)) {
+            Database::$connection = new mysqli("localhost", "root", "Fa@azha1", "final_project", "3306");
         }
     }
 
     public static function iud($q)
     {
-        Connection::setUpConnection();
-        Connection::$connection->query($q);
+        Database::setUpConnection();
+        Database::$connection->query($q);
     }
 
     public static function search($q)
     {
-        Connection::setUpConnection();
-        $resultSet = Connection::$connection->query($q);
+        Database::setUpConnection();
+        $resultSet = Database::$connection->query($q);
         return $resultSet;
     }
 }
